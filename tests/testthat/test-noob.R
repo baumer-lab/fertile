04efe32fd11ca4113b06299bfdef8305e03c7015
Ben Baumer <ben.baumer@gmail.com>
2018-09-26 12:47:44 -0400

    updated tests

--- tests/testthat/test-noob.R
+++ tests/testthat/test-noob.R
@@ -1,13 +1,30 @@
 context("noob")
 
 test_that("checks work", {
-  x <- file_exists_within(test_paths$path)
-  expect_equal(sum(x), 1)
+  expect_equal(fs::file_exists(test_paths$path),
+               test_paths$file_exists, check.names = FALSE)
+  expect_equal(path_within(test_paths$path),
+               test_paths$path_within)
+  expect_equal(file_exists_within(test_paths$path),
+               test_paths$path_within & test_paths$file_exists, check.names = FALSE)
 
+  read_csv_test <- test_paths %>%
+    dplyr::pull(path) %>%
+    purrr::map(purrr::possibly(read_csv, NA, quiet = FALSE)) %>%
+    purrr::map_lgl(tibble::is_tibble)
+  expect_equal(read_csv_test, test_paths$read_csv)
+})
+
+
+test_that("rendering works", {
   dir <- here::here("tests", "project_noob")
   rmd <- fs::dir_ls(dir, regexp = "\\.Rmd$")
   expect_equal(nrow(check(dir)), 2)
   rmarkdown::render(rmd, output_dir = tempdir())
   expect_length(fs::dir_ls(tempdir(), regexp = "\\.html$"), 1)
   path_find_file(rmd)
+})
+
+test_that("shims work", {
+
 })
