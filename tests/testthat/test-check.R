684aff65a02ca88b97997a5e05c96660a2daed33
Ben Baumer <ben.baumer@gmail.com>
2021-01-03 11:34:20 -0500

    made test more isolated

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,90 +1,105 @@
 context("projects")
 
-test_that("has functions work", {
+dir <- test_path("project_noob")
 
-  dir <- test_path("project_noob")
+test_that("has_proj_root works", {
   test_dir <- sandbox(dir)
 
   # delete files we don't want
-
-  r <- dir_ls(test_dir, regexp = "\\.R$")
+  r <- dir_ls(test_dir, regexp = "\\.R$|\\.pdf$")
   file_delete(r)
-  pdf <- dir_ls(test_dir, regexp = "\\.pdf$")
-  file_delete(pdf)
 
   # project roots
-
   expect_true(has_proj_root(test_dir)$state)
   rproj <- dir_ls(test_dir, regexp = "\\.Rproj$")
   file_delete(rproj)
   expect_false(has_proj_root(test_dir)$state)
+})
+
+test_that("has_no_nested_proj_root works", {
 
   test_dir <- sandbox(dir)
 
-  r <- dir_ls(test_dir, regexp = "\\.R$")
+  r <- dir_ls(test_dir, regexp = "\\.R$|\\.pdf$")
   file_delete(r)
-  pdf <- dir_ls(test_dir, regexp = "\\.pdf$")
-  file_delete(pdf)
 
   expect_true(has_no_nested_proj_root(test_dir)$state)
   dir_create(path(test_dir, "R"))
   fake <- path(test_dir, "R", "fake.Rproj")
+  rproj <- dir_ls(test_dir, regexp = "\\.Rproj$")
   file_copy(rproj, fake)
   expect_false(has_no_nested_proj_root(test_dir)$state)
+})
 
+test_that("has_readme works", {
   test_dir <- sandbox(dir)
 
-  r <- dir_ls(test_dir, regexp = "\\.R$")
+  r <- dir_ls(test_dir, regexp = "\\.R$|\\.pdf$")
   file_delete(r)
-  pdf <- dir_ls(test_dir, regexp = "\\.pdf$")
-  file_delete(pdf)
 
   expect_false(has_readme(test_dir)$state)
   readme <- path(test_dir, "README.md")
   file_create(readme)
   expect_true(has_readme(test_dir)$state)
+})
 
+test_that("has_no_lint works", {
+  test_dir <- sandbox(dir)
   expect_true(has_no_lint(test_dir)$state)
+})
 
+test_that("has_clear_build_chain works", {
+  test_dir <- sandbox(dir)
   expect_true(has_clear_build_chain(test_dir)$state)
   file_create(path(test_dir, "second.R"))
   expect_false(has_clear_build_chain(test_dir)$state)
   file_move(path(test_dir, "second.R"), path(test_dir, "02-second.R"))
   file_move(path(test_dir, "simple.Rmd"), path(test_dir, "01-simple.Rmd"))
   expect_true(has_clear_build_chain(test_dir)$state)
+})
 
+test_that("has_tidy works", {
+  test_dir <- sandbox(dir)
   expect_true(has_tidy_media(test_dir)$state)
   expect_true(has_tidy_images(test_dir)$state)
   expect_true(has_tidy_code(test_dir)$state)
   expect_true(has_tidy_raw_data(test_dir)$state)
   expect_true(has_tidy_data(test_dir)$state)
 
+})
+
+test_that("has_tidy_scripts works", {
   test_dir <- sandbox(dir)
 
-  r <- dir_ls(test_dir, regexp = "\\.R$")
+  r <- dir_ls(test_dir, regexp = "\\.R$|\\.pdf$")
   file_delete(r)
-  pdf <- dir_ls(test_dir, regexp = "\\.pdf$")
-  file_delete(pdf)
 
   expect_true(has_tidy_scripts(test_dir)$state)
   file_create(path(test_dir, "second.R"))
   expect_false(has_tidy_scripts(test_dir)$state)
+})
 
+test_that("has_well_commented_code works", {
   comments <- test_path("project_comments")
   temp_dir <- sandbox(comments)
   expect_false(has_well_commented_code(temp_dir)$state)
   expect_equal(length(has_well_commented_code(temp_dir)$error), 1)
+})
 
 
+
+test_that("compilation works", {
   # compilation
-
   noob <- testthat::test_path("project_noob")
   random <- testthat::test_path("project_random")
   random_seed <- testthat::test_path("project_randomseed")
+  miceps <- testthat::test_path("project_miceps")
 
   #data_dir <- sandbox("tests/testthat/data")
 
   r <- dir_ls(noob, regexp = "\\.R$")
+  file_delete(r)
+  r <- dir_ls(miceps, regexp = "\\.R$|\\.pdf$")
   file_delete(r)
 
   #.Random.seed is not recognized unless running RStudio
@@ -97,13 +112,6 @@ test_that("has functions work", {
 
   expect_true(has_no_absolute_paths(noob)$state)
   expect_false(has_only_portable_paths(noob)$state)
-
-  miceps <- testthat::test_path("project_miceps")
-
-  r <- dir_ls(miceps, regexp = "\\.R$")
-  file_delete(r)
-  pdf <- dir_ls(miceps, regexp = "\\.pdf$")
-  file_delete(pdf)
 
   expect_false(has_only_used_files(miceps)$state)
   expect_true(has_only_used_files(random)$state)
--- tests/testthat/test-projects.R
+++ tests/testthat/test-projects.R
@@ -1,75 +1,77 @@
 context("projects")
 
- test_that("project checking works", {
+test_that("noob checking works", {
 
-   # noob
-   dir <- test_path("project_noob")
+  # noob
+  dir <- test_path("project_noob")
   # test_dir <- sandbox(dir)
 
-   session_file <- fs::path(dir, ".software-versions.txt")
-   if (fs::file_exists(session_file)){
-     fs::file_delete(session_file)
-   }
-   expect_false(fs::file_exists(session_file))
+  session_file <- fs::path(dir, ".software-versions.txt")
+  if (fs::file_exists(session_file)) {
+    fs::file_delete(session_file)
+  }
+  expect_false(fs::file_exists(session_file))
 
 
-   proj_render(dir)
-   expect_equal(length(render_log_report(dir)$path), 9)
-   expect_equal(tail(render_log_report(dir)$path, 1), "LAST RENDERED")
+  proj_render(dir)
+  expect_equal(length(render_log_report(dir)$path), 9)
+  expect_equal(tail(render_log_report(dir)$path, 1), "LAST RENDERED")
 
 
-   expect_true(fs::file_exists(session_file))
+  expect_true(fs::file_exists(session_file))
 
-   x <- proj_check(dir)
-   expect_gt(nrow(x), 1)
+  x <- proj_check(dir)
+  expect_gt(nrow(x), 1)
 
-   y <- proj_check_some(dir, ends_with("root"))
-   expect_equal(nrow(y), 2)
+  y <- proj_check_some(dir, ends_with("root"))
+  expect_equal(nrow(y), 2)
 
-   expect_true(has_proj_root(dir)$state)
-   expect_false(has_readme(dir)$state)
-   expect_true(has_tidy_media(dir)$state)
-   expect_true(has_tidy_images(dir)$state)
-   expect_true(has_tidy_code(dir)$state)
-   expect_true(has_tidy_raw_data(dir)$state)
-   expect_true(has_tidy_data(dir)$state)
-   expect_true(has_tidy_scripts(dir)$state)
+  expect_true(has_proj_root(dir)$state)
+  expect_false(has_readme(dir)$state)
+  expect_true(has_tidy_media(dir)$state)
+  expect_true(has_tidy_images(dir)$state)
+  expect_true(has_tidy_code(dir)$state)
+  expect_true(has_tidy_raw_data(dir)$state)
+  expect_true(has_tidy_data(dir)$state)
+  expect_true(has_tidy_scripts(dir)$state)
 
-   #data_dir <- sandbox(test_path("data"))
+  # data_dir <- sandbox(test_path("data"))
 
-   #expect_true(has_no_randomness(test_dir)$state)
-   expect_true(has_no_absolute_paths(dir)$state)
-   expect_false(has_only_portable_paths(dir)$state)
+  # expect_true(has_no_randomness(test_dir)$state)
+  expect_true(has_no_absolute_paths(dir)$state)
+  expect_false(has_only_portable_paths(dir)$state)
 
 
 
-   expect_warning(proj_analyze_files(dir), "README")
-   expect_warning(x <- proj_test(dir), "README")
+  expect_warning(proj_analyze_files(dir), "README")
+  expect_warning(x <- proj_test(dir), "README")
 
-   expect_length(fs::dir_ls(tempdir(), regexp = "simple.html$"), 1)
-   expect_equal(nrow(x$packages), 3)
-   # .Rbuildignore says to ignore .Rproj files!
-   expect_equal(nrow(dplyr::filter(x$files, ext != "Rproj")), 1)
-   expect_equal(nrow(x$suggestions),1)
-   expect_equal(nrow(x$paths), 2)
+  expect_length(fs::dir_ls(tempdir(), regexp = "simple.html$"), 1)
+  expect_equal(nrow(x$packages), 3)
+  # .Rbuildignore says to ignore .Rproj files!
+  expect_equal(nrow(dplyr::filter(x$files, ext != "Rproj")), 1)
+  expect_equal(nrow(x$suggestions), 1)
+  expect_equal(nrow(x$paths), 2)
 
-   proj_move_files(x$suggestions, execute = FALSE)
-   expect_length(fs::dir_ls(dir, type = "dir"), 0)
-   proj_move_files(x$suggestions, execute = TRUE)
-   expect_length(fs::path_file(fs::dir_ls(dir, type = "dir")), 1)
+  proj_move_files(x$suggestions, execute = FALSE)
+  expect_length(fs::dir_ls(dir, type = "dir"), 0)
+  proj_move_files(x$suggestions, execute = TRUE)
+  expect_length(fs::path_file(fs::dir_ls(dir, type = "dir")), 1)
+})
 
-   # miceps
-   dir <- test_path("project_miceps")
-   x <- proj_test(dir)
+test_that("miceps checking works", {
 
-   expect_equal(nrow(x$packages), 9)
-   expect_equal(nrow(dplyr::filter(x$files, ext != "Rproj")), 8)
-   expect_equal(nrow(x$suggestions), 7)
-   expect_equal(nrow(x$paths), 0)
+  # miceps
+  dir <- test_path("project_miceps")
+  x <- proj_test(dir)
 
-   proj_move_files(x$suggestions, execute = FALSE)
-   expect_length(fs::dir_ls(dir, type = "dir"), 0)
-   proj_move_files(x$suggestions, execute = TRUE)
-   expect_length(path_file(fs::dir_ls(dir, type = "dir")), 3)
- })
+  expect_equal(nrow(x$packages), 9)
+  expect_equal(nrow(dplyr::filter(x$files, ext != "Rproj")), 8)
+  expect_equal(nrow(x$suggestions), 7)
+  expect_equal(nrow(x$paths), 0)
 
+  proj_move_files(x$suggestions, execute = FALSE)
+  expect_length(fs::dir_ls(dir, type = "dir"), 0)
+  proj_move_files(x$suggestions, execute = TRUE)
+  expect_length(path_file(fs::dir_ls(dir, type = "dir")), 3)
+})
